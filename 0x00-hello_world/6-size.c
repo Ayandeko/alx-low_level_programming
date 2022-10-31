95105819cb1e92b8efe4d890bc8828a93c2613a8
Ayandeko <ayandekoayotunde@gmail.com>
2022-10-31 16:05:24 -0700

    checking sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int main(void)
 	long a;
 	long long b;
 
-	printf("Size of a char: %c byte(s)", sizeof(char));
-	printf("Size of an int: %d byte(s)", sizeof(int));
-	printf("Size of a long int: %ld byte(s)", sizeof(a));
-	printf("Size of a long long int: %lld byte(s)", sizeof(b));
-	printf("Size of a float: %f byte(s)", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(a));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(b));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }

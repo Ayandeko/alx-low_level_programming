a8889e93a76db08283c222fc3c5f3ed2e16fff9a
Ayandeko <ayandekoayotunde@gmail.com>
2022-11-09 08:01:46 -0800

    initial commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,13 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-
-int main(void);
-
+/**
+  * main - entry point
+  *
+  * Return: is zero
+  *
+  */
+int main(void)
 {
 		int n;
 
@@ -16,12 +20,10 @@ int main(void);
 				if (n > 0)
 				{
 					printf("%d is positive\n", n);
-				}
-				else if (n == 0)
+				} else if (n == 0)
 				{
 					printf("%d is zero\n", n);
-				}
-				else if (n < 0)
+				} else if (n < 0)
 				{
 					printf("%d is negetive\n", n);
 				}

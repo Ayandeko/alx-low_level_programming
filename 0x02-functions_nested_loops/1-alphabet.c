9061c7852298f91ea72fdd7441387f56a1569cc3
Ayandeko <ayandekoayotunde@gmail.com>
2022-11-16 05:33:44 -0800

    printing alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * void print_alphabets - Entry point
+  * Return: void
+  */
+void print_alphabets(void)
+{
+	int ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		putchar(ch);
+	putchar('\n');
+}

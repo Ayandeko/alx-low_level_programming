dcb7241b080f3dc313c3462e787f8ef9d0a010d5
Ayandeko <ayandekoayotunde@gmail.com>
2022-11-16 06:08:21 -0800

    printing 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - void
+  * Return: void
+  */
+void print_alphabet_x10(void)
+{
+	int ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		for (ch = 1 ; ch <= 10 ; ch++)
+			_putchar(ch);
+	}
+	putchar('\n');
+}

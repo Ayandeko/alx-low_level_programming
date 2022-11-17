861fed850972ea7b51460896a17568f4574a8977
Ayandeko <ayandekoayotunde@gmail.com>
2022-11-17 00:27:30 -0800

    checking lower cases

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,12 @@
 #include "main.h"
 /**
   * int_is lower - Entry point
+  * @c: a character argument
   * Return: 1 for success and 0 for fail
   */
 int _islower(int c)
 {
+	int = 0;
+
 	return (c >= 'a' && c <= 'z');
 }

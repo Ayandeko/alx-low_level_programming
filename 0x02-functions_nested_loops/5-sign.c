d011e2f4d1747f7e78192b5942a2b49fe7e64c82
Ayandeko <ayandekoayotunde@gmail.com>
2022-11-17 00:59:03 -0800

    checking digits

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+  * print_sign - Entry point
+  * @n: a character argument
+  * Return: 1 or 0 when successful and -1 when failed
+  */
+int print_sign(int n)
+{
+	return (n >= 0);
+}

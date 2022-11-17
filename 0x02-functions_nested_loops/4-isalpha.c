e62a05e1b477e719eacb708b0421b02febbc80fe
Ayandeko <ayandekoayotunde@gmail.com>
2022-11-17 00:46:25 -0800

    checking cases

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+  * _isalpha - Entry point
+  * @c: a character argument
+  * Return: 1 when successful and 0 when failed
+  */
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+}

f6e2de06aacfaa74b0af92516b2fead92e5ccabe
Ayandeko <ayandekoayotunde@gmail.com>
2022-11-20 17:26:47 -0800

    using putchar to print digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * print_numbers - Entry point
+  * Return: void
+  */
+void print_numbers(void)
+{
+	int b = 0;
+
+	for (b = 0 ; b <= 9 ; b++)
+		_putchar('b');
+	_putchar('\n');
+}

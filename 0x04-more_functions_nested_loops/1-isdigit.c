7fbb9dba08605a5f944bdb9653aa0da2a9469bbd
Ayandeko <ayandekoayotunde@gmail.com>
2022-11-20 17:12:34 -0800

    checking for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * _isdigit - Entry point
+  * Return: 1, 0
+  * @c: a character argument
+  */
+int _isdigit(int c)
+{
+	if ((c >= 0) && (c <= 9))
+
+		return (1);
+
+	return (0);
+}

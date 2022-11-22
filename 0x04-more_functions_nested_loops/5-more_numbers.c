1d86b2d0836304676d610c75f1e0a79c2a07f017
Ayandeko <ayandekoayotunde@gmail.com>
2022-11-22 06:00:01 -0800

    more lines

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * more_numbers - Entry point
+  * Return: void
+  */
+void more_numbers(void)
+{
+	int m;
+	int n;
+
+	for (m = 0 ; m < 10 ; m++)
+	{
+		for (n = 0 ; n < = 14 ; n++)
+			_putchar(n);
+		_putchar('\n');
+	}
+}

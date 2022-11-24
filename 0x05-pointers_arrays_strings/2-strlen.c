f9f41c47072b129bc53b72796c14016f16ad7b86
Ayandeko <ayandekoayotunde@gmail.com>
2022-11-24 07:42:09 -0800

    counting string character

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * _strlen - Entry point
+  * Return: Result
+  * @x: pointer value
+  */
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0 ; s[x] != '\0' ; x++)
+	{
+		continue;
+	}
+	return (x);
+}
